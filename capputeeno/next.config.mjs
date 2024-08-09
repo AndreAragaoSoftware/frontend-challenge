77b79b77b17d89198f10763499ebb611af836007
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-08-09 13:10:39 +0100

    feat: creating header

--- capputeeno/next.config.mjs
+++ capputeeno/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    compiler: {
+        styledComponents: true // Utiliza em toda a aplicacao
+    }
+};
 
 export default nextConfig;
