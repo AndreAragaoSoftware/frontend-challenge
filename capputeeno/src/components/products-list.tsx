6535433972434cd314975ee9626cb66bb1969851
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-08-19 17:41:41 +0100

    feat: add pagination

--- capputeeno/src/components/products-list.tsx
+++ capputeeno/src/components/products-list.tsx
@@ -5,12 +5,14 @@ import { ProductCard } from "./product-card";
 import styled from "styled-components";
 
 const ListContainer = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 256px);
-    grid-gap: 32px;
-    max-width: 100%;
-    margin-top: 32px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(355px, 1fr));
+  grid-gap: 32px;
+  width: 100%;
+  margin-top: 32px;
+  justify-content: space-around; /* ou space-between, dependendo do efeito desejado */
 `
+
 
 export function ProductList() {
     const { data } = useProducts();
